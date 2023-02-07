85a34ebd08382342a2b73500b6f607820aae6360
jyx <915887751@jyx.com>
2023-02-07 18:00:25 +0800

    1

---
+++ src/main/java/com/jyx/git/GitTest.java
@@ -0,0 +1,18 @@
+package com.jyx.git;
+
+/**
+ * ClassName:git-test
+ * Package:com.jyx.git
+ * Description:
+ *
+ * @Author:Rose
+ * @Create:2023/2/7 - 17:59
+ * @Version:v1.0
+ */
+public class GitTest {
+    public static void main(String[] args) {
+
+        System.out.println("1");
+
+    }
+}
